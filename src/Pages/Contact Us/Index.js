81957f259edfe2a14fa9ab6bf387337f7dda7a68
Carvalh0R0b3rt <carvalhrobert561@gmail.com>
2025-08-15 20:38:44 -0300

    Setting up Multi-Page

--- src/Pages/Contact Us/Index.js
+++ src/Pages/Contact Us/Index.js
@@ -1,3 +1,5 @@
 import './Page.css'
+import FooterMenu from "./Componentes/Footer/index.js";
+import HeaderMenu from "./Componentes/Header/index.js";
 
 export default Contact_Us
--- src/Pages/NotFoundPage/index.js
+++ src/Pages/NotFoundPage/index.js
@@ -1,4 +1,6 @@
 import './Page.css'
+import FooterMenu from "./Componentes/Footer/index.js";
+import HeaderMenu from "./Componentes/Header/index.js";
 
 
 export default NotFoundPage
